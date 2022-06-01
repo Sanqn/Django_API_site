2565d1d9b5953ed770434deeb55459f005950465
Sanqn <sandisvert@gmail.com>
2022-06-01 20:59:28 +0300

    pagination

--- api/core/views.py
+++ api/core/views.py
@@ -2,7 +2,14 @@ from rest_framework import viewsets
 from .serializers import PostSerializer
 from .models import Post
 from rest_framework import permissions
+from rest_framework import pagination
 from rest_framework.response import Response
+
+
+class PageNumberSetPagination(pagination.PageNumberPagination):
+    page_size = 6
+    page_size_query_param = 'page_size'
+    ordering = 'created_at'
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -10,3 +17,4 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     lookup_field = 'slug'
     permission_classes = [permissions.AllowAny]
+    pagination_class = PageNumberSetPagination
