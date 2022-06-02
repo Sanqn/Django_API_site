29d2baa8edbb71a7e3ba68d0cfafbfcbc4ceee0b
Sanqn <sandisvert@gmail.com>
2022-06-02 11:42:12 +0300

    create_serializer_for_last_posts

--- api/core/urls.py
+++ api/core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, TagDetailView, TagView
+from .views import PostViewSet, TagDetailView, TagView, AsideView
 
 router = DefaultRouter()
 router.register('posts', PostViewSet, basename='posts')
@@ -9,4 +9,5 @@ urlpatterns = [
     path("", include(router.urls)),
     path("tags/<slug:tag_slug>/", TagDetailView.as_view(), name='tags'),
     path("tags/", TagView.as_view(), name='all_tags'),
+    path("aside/", AsideView.as_view(), name='aside'),
 ]
